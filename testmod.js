bf193fd4d4f871b06e7144704058e376d93e3bcf
Levlio12345 <113808206+Levlio123456@users.noreply.github.com>
2025-10-03 07:28:36 -0400

    Update testmod.js

--- testmod.js
+++ testmod.js
@@ -1,7 +1,8 @@
 elements.plutonium = {
 	color: ["#cf722b", "#ba6625", "#c96d26", "#ba6523", "#ad662f", "#c96e28", "#cf7734", "#ba692b"],
-	behavior: behaviors.WALL,
+	behavior: behaviors.POWDER,
 	category: "powders",
 	state: "solid",
-	density: 2150
+	density: 2150,
+	hardness: 1.0;
 }
